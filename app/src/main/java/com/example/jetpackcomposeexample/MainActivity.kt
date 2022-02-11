ae56761bb5b42df08868165b0805d3a7f7875e3c
Miguel Ángel Delgado <migue@miguelandelgado.com>
2022-02-11 13:26:01 +0100

    Video 4
    Hecho hasta el video 4

--- app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,14 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
@@ -27,19 +28,19 @@ import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposeexample.ui.theme.JetPackComposeExampleTheme
 
 private val messages: List<MyMessage> = listOf(
-    MyMessage("Hola Jetpack Composer 1", "¿estas preparado 1"),
-    MyMessage("Hola Jetpack Composer 2", "¿estas preparado 2"),
-    MyMessage("Hola Jetpack Composer 3", "¿estas preparado 3"),
-    MyMessage("Hola Jetpack Composer 4", "¿estas preparado 4"),
-    MyMessage("Hola Jetpack Composer 5", "¿estas preparado 5"),
-    MyMessage("Hola Jetpack Composer 6", "¿estas preparado 6"),
-    MyMessage("Hola Jetpack Composer 7", "¿estas preparado 7"),
-    MyMessage("Hola Jetpack Composer 8", "¿estas preparado 8"),
-    MyMessage("Hola Jetpack Composer 9", "¿estas preparado 9"),
-    MyMessage("Hola Jetpack Composer 10", "¿estas preparado 10"),
-    MyMessage("Hola Jetpack Composer 11", "¿estas preparado 11"),
-    MyMessage("Hola Jetpack Composer 12", "¿estas preparado 12"),
-    MyMessage("Hola Jetpack Composer 13", "¿estas preparado 13"),
+    MyMessage("Hola Jetpack Composer 1", "¿estas preparado 1 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."),
+    MyMessage("Hola Jetpack Composer 2", "¿estas preparado 2 Lorem Ipsum is simply dummy text of the printing and typesetting industry."),
+    MyMessage("Hola Jetpack Composer 3", "¿estas preparado 3 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Hola Jetpack Composer 4", "¿estas preparado 4 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Hola Jetpack Composer 5", "¿estas preparado 5 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
+    MyMessage("Hola Jetpack Composer 6", "¿estas preparado 6 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Hola Jetpack Composer 7", "¿estas preparado 7 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."),
+    MyMessage("Hola Jetpack Composer 8", "¿estas preparado 8 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Hola Jetpack Composer 9", "¿estas preparado 9 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Hola Jetpack Composer 10", "¿estas preparado 10 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Hola Jetpack Composer 11", "¿estas preparado 11 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Hola Jetpack Composer 12", "¿estas preparado 12 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    MyMessage("Hola Jetpack Composer 13", "¿estas preparado 13 Lorem Ipsum."),
 )
 
 class MainActivity : ComponentActivity() {
@@ -88,7 +89,12 @@ fun MyImage(){
 
 @Composable
 fun MyTexts(message: MyMessage){
-    Column(modifier = Modifier.padding(8.dp)){
+
+    var expanded by remember { mutableStateOf( false)}
+
+    Column(modifier = Modifier.padding(8.dp).clickable {
+        expanded = !expanded
+    }){
         MyText(
             message.title,
             MaterialTheme.colors.primary,
@@ -97,13 +103,14 @@ fun MyTexts(message: MyMessage){
         MyText(
             message.body,
             MaterialTheme.colors.onBackground,
-            MaterialTheme.typography.subtitle2)
+            MaterialTheme.typography.subtitle2,
+            if (expanded) Int.MAX_VALUE else  1)
     }
 
 }
 @Composable
-fun MyText(text: String, color: Color, style: TextStyle){
-    Text(text, color = color, style = style)
+fun MyText(text: String, color: Color, style: TextStyle, lines: Int = Int.MAX_VALUE){
+    Text(text, color = color, style = style, maxLines = lines)
 }
 
 @Preview(showSystemUi = true)
